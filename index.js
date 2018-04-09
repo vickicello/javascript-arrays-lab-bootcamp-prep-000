35add7c8e28e98a37a6da5723e69a535f18d3016
Vicki Hamm <vicki.cello@gmail.com>
2018-04-09 22:38:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+}
+
+function accessElementInArray(kittens, index) {
+  return kittens[index];
+}
+
+function destructivelyRemoveFirstKitten(array) {
+  kittens.shift([0]);
+}
+
+function appendKitten(kittens, name) {
+  return (...kittens, name);
+}
 
 
 
